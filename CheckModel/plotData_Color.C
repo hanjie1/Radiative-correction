a7600430651b4e721fe63390245fa53753a733a8
Hanjie Liu <hanjie@mrtcol.jlab.org>
2018-10-24 11:59:08 -0400

    model comparsion 2

--- CheckModel/plotData_Color.C
+++ CheckModel/plotData_Color.C
@@ -1,7 +1,7 @@
 #define MAXNUM 3000
 #define PI TMath::Pi()
 #define Mp 0.938272
-void plotData()
+void plotData_Color()
 {
     int Z,A;
     cout<<"Input Z: ";
@@ -39,14 +39,6 @@ void plotData()
     }
     infile1.close();
     
-    ofstream outfile;
-    outfile.open("4He_Q2_xbj.dat");
-
-    ofstream outfile1;
-    outfile1.open("4He_50.dat");
-    ofstream outfile2;
-    outfile2.open("data/4He_50.dat");
-
     nn=nn-1;
     Double_t Q2[MAXNUM]={0.0},xbj[MAXNUM]={0.0},W2[MAXNUM]={0.0};
     Double_t Eprime[MAXNUM]={0.0};
@@ -55,21 +47,13 @@ void plotData()
         xbj[ii]=Q2[ii]/(2.0*Mp*Nu[ii]);
 	W2[ii]=Mp*Mp+2*Mp*Nu[ii]-Q2[ii];
         Eprime[ii]=E0[ii]-Nu[ii];
-        outfile<<E0[ii]<<"  "<<Theta[ii]<<"  "<<Q2[ii]<<"  "<<xbj[ii]<<endl; 
-        if(E0[ii]==5.766&&Theta[ii]==50)outfile1<<left<<fixed<<setprecision(4)<<setw(6)<<E0[ii]<<" "<<setw(6)<<Eprime[ii]<<" "<<setw(6)<<Theta[ii]<<endl;
-        if(E0[ii]==5.766&&Theta[ii]==50)outfile2<<E0[ii]<<" "<<Eprime[ii]<<" "<<Theta[ii]<<" "<<XS[ii]<<" "<<XS_err[ii]<<endl;
     }
-    outfile.close();
-
-    outfile1.close();
-    outfile2.close();
-
     TCanvas *c1=new TCanvas("c1");
     TGraphErrors *gXbj=new TGraphErrors();
     int npoint=0;
     for(int ii=0;ii<nn;ii++){
-	if(E0[ii]!=5.766)continue;
-	if(Theta[ii]!=50)continue;
+	if(E0[ii]!=2.020)continue;
+	if(Theta[ii]!=15.022)continue;
 	gXbj->SetPoint(npoint,xbj[ii],Q2[ii]);
         gXbj->SetPointError(npoint,0,0);
         npoint++;
@@ -77,14 +61,14 @@ void plotData()
     gXbj->SetMarkerStyle(8);
     gXbj->SetMarkerColor(4);
     gXbj->Draw("AP");
-    gXbj->SetTitle("Q2 vs. xbj(E0=5.766,Theta=50);xbj;Q2");
+    gXbj->SetTitle("Q2 vs. xbj(E0=2.020,Theta=15.022);xbj;Q2");
 
     TCanvas *c2=new TCanvas("c2");
     TGraphErrors *gNu=new TGraphErrors();
     npoint=0;
     for(int ii=0;ii<nn;ii++){
-	if(E0[ii]!=5.766)continue;
-	if(Theta[ii]!=50)continue;
+	if(E0[ii]!=2.020)continue;
+	if(Theta[ii]!=15.022)continue;
 	gNu->SetPoint(npoint,Nu[ii],XS[ii]);
         gNu->SetPointError(npoint,0,XS_err[ii]);
         npoint++;
@@ -95,9 +79,22 @@ void plotData()
     gNu->SetTitle("XS vs. nu;nu;XS");
 
     TCanvas *c3=new TCanvas("c3");
-    TGraph *gQ2E0=new TGraph(nn,xbj,XS);
-    gQ2E0->SetMarkerStyle(8);
-    gQ2E0->Draw("AP");
-    gQ2E0->SetTitle("Q2 vs. xbj;xbj;Q2");
+    TGraphErrors *gXS=new TGraphErrors();
+    npoint=0;
+    for(int ii=0;ii<nn;ii++){
+	if(E0[ii]!=2.020)continue;
+	if(Theta[ii]!=15.022)continue;
+	gXS->SetPoint(npoint,xbj[ii],XS[ii]);
+        gXS->SetPointError(npoint,0,XS_err[ii]);
+        npoint++;
+    }
+    gXS->SetMarkerStyle(8);
+    gXS->SetMarkerColor(4);
+    gXS->Draw("AP");
+    gXS->SetTitle("XS vs. xbj;xbj;XS");
+
+
+
+
 
 }
